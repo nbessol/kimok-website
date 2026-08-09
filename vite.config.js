cc5612c0ce9b4dadbdc996393039285b5763570d
heynizest <nicolas@zestmeup.com>
2026-08-09 18:19:27 +0200

    fix deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kimok-website/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
         loadPaths: ['src/variables'],
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy/index.html'),
       },
     },
   },
